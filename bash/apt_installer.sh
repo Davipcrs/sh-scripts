8e4028da7b38161304129ca282be5708db0fab53
Davipcrs <davipernalonga30@gmail.com>
2023-08-25 22:44:44 -0300

    add apt installers file

---
+++ bash/apt_installer.sh
@@ -0,0 +1,15 @@
+sudo add-apt-repository universe -y 
+
+sudo apt -y  install git
+sudo apt -y  install gnome-tweaks
+sudo apt -y  install wget
+sudo apt -y  install flatpak
+sudo apt -y  install snapd
+sudo apt -y  install neofetch
+sudo apt -y  install htop
+sudo apt -y  install plocate
+sudo apt -y  install locate
+
+
+#Android Studio support
+sudo apt -y  install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
--- bash/installer.sh
+++ bash/installer.sh
@@ -5,21 +5,9 @@ echo "instalador executado!"
 bash system_maintainer.sh
 
 #repository adding
-sudo add-apt-repository universe -y
-
 #apt installers
-sudo apt -y install git
-sudo apt -y install gnome-tweaks
-sudo apt -y install wget
-sudo apt -y install flatpak
-sudo apt -y install snapd
-sudo apt -y install neofetch
-sudo apt -y install htop
-sudo apt -y install plocate
-sudo apt -y install locate
-#Android Studio support
 
-sudo apt -y install libc6:i386 libncurses5:i386 libstdc++6:i386 lib32z1 libbz2-1.0:i386
+bash apt_installer.sh
 
 #wget installers
 #uncomment this 
