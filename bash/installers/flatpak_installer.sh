c144736f2c81844505191677a00a391100410b35
Davipcrs <davipernalonga30@gmail.com>
2023-09-06 21:10:04 -0300

    wine

--- bash/installers/flatpak_installer.sh
+++ bash/installers/flatpak_installer.sh
@@ -4,3 +4,4 @@ flatpak install -y flathub com.discordapp.Discord
 flatpak install -y flathub com.obsproject.Studio
 flatpak install -y flathub org.videolan.VLC
 flatpak install -y flathub org.remmina.Remmina
+flatpak install -y flathub io.qt.Designer
